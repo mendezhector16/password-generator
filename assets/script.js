3344b1c28e931a4ea6fa212fc6acdb9dde0bc3a0
mendezhector16 <mendezhector16@gmail.com>
2022-05-15 16:08:04 -0500

    prompt asking for number of characters

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,34 @@
 // Assignment code here
+var generatePassword = function() {
+  //ask person how long they want the password
+  var promptLength = window.prompt("Would you like 8, 10, or 12 characters in your password? Please type the numbers 8, 10, or 12 to chose.")
+
+  if (promptLength === "8") {
+    //if 8 characters go to  generateEight()
+
+
+  }
+  else if (promptLength === "10") {
+    //if 10 charcters go to generateTen()
+
+  }
+  else if (promptLength === "12") {
+    //if 12 characters go to generateTwelve
+
+
+  }
+  else if ( promptLength === "" || promptLength === null) {
+    window.alert("Please provide a valid answer! Try again.");
+    return generatePassword();
+  }
+  else {
+    window.alert("Please provide a valid answer! Try again.");
+    return generatePassword();
+  }
+};
+
+
+
 
 
 // Get references to the #generate element
