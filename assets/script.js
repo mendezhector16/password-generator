e5a43b26cf2d9ae300cbb97caf4c6bddd25bb4e6
mendezhector16 <mendezhector16@gmail.com>
2022-06-08 14:17:57 -0500

    added options for confirm prompts

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,6 @@
 var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numb = [0,1,2,3,4,5,6,7,8,9];
 var symbol = ["!","@","#","$","%","^","&","*","_","-","+","=",".","?"];
-
 //var to randomize characters
 var randomNumber = function(min,max) {
   var value = Math.floor(Math.random() * (max - min + 1) +min);
@@ -12,24 +11,11 @@ var randomNumber = function(min,max) {
 };
 
 //var to create upper case characters
-var toUpperCase = function(x) {
-  return x.toUpperCase();
-};
+var alphaUpperCase = alpha.map(alpha => alpha.toUpperCase());
 
-//var to confirm use of numbers
-var useNumbers = window.confirm ("Using numbers in your password may help with security. Would you life to include numbers?");
+var userResponse
 
-//var to confirm use of lowercase letters
-var useLowerCase = window.confirm ("Using lower case letters in your password may help with security. Would you like to include lower case letters?");
-
-//var to confirm use of upper case characters
-var useUpperCase = window.confirm ("Using upper case letters in your password may help with security. Would you like to include upper case ltters? ");
-
-//var to confirm special charecters
-var useSpecialCharacters = window.confirm ("Using special charecters in your password may help with security. Would you like to include special characters?")
-
-
-var setPasswordLength = function() {
+var generatePassword = function() {
   //ask person how long they want the password
   var passwordLength = window.prompt("How many characters would you like your password to be? Please choose a number between 8-128.")
  
@@ -42,30 +28,62 @@ var setPasswordLength = function() {
     generatePassword();
   }
   else {
-    setUseNumbers();
-    return passwordLength;
+    //var to confirm use of numbers
+    var useNumbers = window.confirm ("Using numbers in your password may help with security. Would you life to include numbers?");
+
+    //var to confirm use of lowercase letters
+    var useLowerCase = window.confirm ("Using lower case letters in your password may help with security. Would you like to include lower case letters?");
+
+    //var to confirm use of upper case characters
+    var useUpperCase = window.confirm ("Using upper case letters in your password may help with security. Would you like to include upper case ltters? ");
+
+    //var to confirm special charecters
+    var useSpecialCharacters = window.confirm ("Using special charecters in your password may help with security. Would you like to include special characters?")
+
+    // return passwordLength;
+  };
+
+  if (!useNumbers && !useLowerCase && !useUpperCase && !useSpecialCharacters) {
+    userResponse = window.alert ("No password able to be generated, please make a valid choice.")
   }
+  //all true
+  else if (useNumbers && useLowerCase && useUpperCase && useSpecialCharacters) {
+    userResponse = numb.concat(alpha,alphaUpperCase, symbol);
+    console.log(userResponse);
+  }
+  //three are true
+  else if (useNumbers && useLowerCase && useUpperCase) {
+    userResponse = numb.concat(alpha,alphaUpperCase);
+    console.log(userResponse);
+  }
+  else if (useNumbers && useLowerCase && useSpecialCharacters) {
+    userResponse = numb.concat(alpha, symbol);
+    console.log(userResponse);
+  }
+  else if (useNumbers && useUpperCase && useSpecialCharacters) {
+    userResponse = numb.concat(alphaUpperCase, symbol);
+    console.log(userResponse);
+  }
+  else if (useLowerCase && useUpperCase && useSpecialCharacters) {
+    userResponse = alpha.concat(alphaUpperCase, symbol);
+    console.log(userResponse);
+  }
+  //two are ture
+  //one is true
+
 };
 
+// var generatePassword = function() {
+//   var password = ""
+//   var passwordLength = setPasswordLength();
 
-
-//create confirm prompt for uppercase characters called characterUpper
-
-//create vonprompt for numbers
-
-// create prompt for special characters
-
-var generatePassword = function() {
-  var password = ""
-  var passwordLength = setPasswordLength();
-
-  for (let i = 0; i < passwordLength; i++) {
-    var randomCharacterAlpha = alpha.charAt(randomNumber(0, alpha.length));
-    var randomCharacterNumbers = numb.charAt(randomNumber(0,numb.length));
-    password = password + (randomCharacterAlpha) + (randomCharacterNumbers);
-  }
-  return password
-}
+  // for (let i = 0; i < passwordLength; i++) {
+  //   var randomCharacterAlpha = alpha.charAt(randomNumber(0, alpha.length));
+  //   var randomCharacterNumbers = numb.charAt(randomNumber(0,numb.length));
+  //   password = password + (randomCharacterAlpha) + (randomCharacterNumbers);
+  // }
+  // return password
+// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
